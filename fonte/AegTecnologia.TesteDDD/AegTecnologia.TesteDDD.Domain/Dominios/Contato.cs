b58ada25f8df032df47ebc4a7c4d64ce6158c339
Anderson dos Santos Ferreira <anderson.ferreira@pasqualisolution.com.br>
2019-07-15 09:42:31 -0300

    estrutura inicial

---
+++ fonte/AegTecnologia.TesteDDD/AegTecnologia.TesteDDD.Domain/Dominios/Contato.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AegTecnologia.TesteDDD.Domain.Dominios
+{
+    class Contato
+    {
+    }
+}
---
+++ fonte/AegTecnologia.TesteDDD/AegTecnologia.TesteDDD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AegTecnologia.TesteDDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
