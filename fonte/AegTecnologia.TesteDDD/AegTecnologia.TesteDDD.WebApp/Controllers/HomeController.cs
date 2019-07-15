c7a81aaf7639d8c39a1742bea7c6ab72cbe1236e
Anderson dos Santos Ferreira <anderson.ferreira@pasqualisolution.com.br>
2019-07-15 16:23:46 -0300

    alteracoes por que eu quis

---
+++ fonte/AegTecnologia.TesteDDD/AegTecnologia.TesteDDD.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AegTecnologia.TesteDDD.WebApp.Models;
+
+namespace AegTecnologia.TesteDDD.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ fonte/AegTecnologia.TesteDDD/AegTecnologia.TesteDDD.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AegTecnologia.TesteDDD.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
