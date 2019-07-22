54e3c50f93e74434bab224b94cfb7fd40e81a405
Anderson Ferreira <anderson.ferreira@interfile.com.br>
2019-07-22 17:32:20 -0300

    inclusao de base de EF

---
+++ fonte/AegTecnologia.TesteDDD/AegTecnologia.TesteDDD.Infra/EF/Configurations/ContatoConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AegTecnologia.TesteDDD.Domain.Dominios;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AegTecnologia.TesteDDD.Infra.EF.Configurations
+{
+    public class ContatoConfiguration : IEntityTypeConfiguration<Contato>
+    {
+        public void Configure(EntityTypeBuilder<Contato> builder)
+        {
+            
+        }
+    }
+}
---
+++ fonte/AegTecnologia.TesteDDD/AegTecnologia.TesteDDD.Infra/EF/EFContext.cs
@@ -0,0 +1,32 @@
+﻿using AegTecnologia.TesteDDD.Domain.Dominios;
+using Microsoft.EntityFrameworkCore.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AegTecnologia.TesteDDD.Infra.EF.Configurations;
+
+namespace AegTecnologia.TesteDDD.Infra.EF
+{
+    //referencia: https://www.devmedia.com.br/entity-framework-core-criando-bases-de-dados-com-migrations/36776
+    public class EFContext : DbContext
+    {
+        public EFContext() { }
+
+        public EFContext(DbContextOptions<EFContext> opcoes)
+            : base(opcoes)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration<Contato>(new ContatoConfiguration());
+        }
+
+    }
+
+
+}
